648b864ac82d7f68aae31b5744c67f83879fcd39
Joshua Martin <ursa8minority@gmail.com>
2023-07-10 14:53:40 -0600

    feat: fixed scroll display on main page

--- src/MovieContainer.js
+++ src/MovieContainer.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import propTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 const MovieContainer = ({ movies, onMovieClick }) => {
   return (
-    <div className="movie-list">
-      {movies.map((movie) => (
-        <div
-          key={movie.id}
-          className="movie-item"
-          onClick={() => onMovieClick(movie)}
-        >
-          <img src={movie.poster_path} alt={movie.title} />
-          <h2>{movie.title}</h2>
-          <p>Average Rating: {movie.average_rating}</p>
-          <p>Release Date: {movie.release_date}</p>
-        </div>
-      ))}
+    <div className="movie-container">
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <div
+            key={movie.id}
+            className="movie-item"
+            onClick={() => onMovieClick(movie)}
+          >
+            <img src={movie.poster_path} alt={movie.title} />
+            <h2>{movie.title}</h2>
+            <p>Average Rating: {movie.average_rating}</p>
+            <p>Release Date: {movie.release_date}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 MovieContainer.propTypes = {
-  movies: propTypes.array.isRequired,
-  onMovieClick: propTypes.func.isRequired,
-}
+  movies: PropTypes.array.isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
 
 export default MovieContainer;
-
